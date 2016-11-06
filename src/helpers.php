3783cc6fbaa4eb828977e0102ff1df9dc344698c
CaribouFuté <frederic.chiasson@gmail.com>
2016-11-05 20:31:41 -0400

    Added helpers

--- src/helpers.php
+++ src/helpers.php
@@ -1,8 +1,9 @@
 <?php
 
 if (!function_exists('locale_route')) {
+
     /**
-     * Generate the URL to a named route.
+     * Generate the URL to given locale and route name.
      *
      * @param  string  $name
      * @param  array   $parameters
@@ -12,5 +13,31 @@ if (!function_exists('locale_route')) {
     function locale_route($locale = null, $name = null, $parameters = [], $absolute = true)
     {
         return app('locale-url')->localeRoute($locale, $name, $parameters, $absolute);
+    }
+
+    /**
+     * Generate the URL for same route name but different locale
+     *
+     * @param  string  $name
+     * @param  array   $parameters
+     * @param  bool    $absolute
+     * @return string
+     */
+    function other_locale($locale, $parameters = [], $absolute = true)
+    {
+        return app('locale-url')->localeRoute($locale, null, $parameters, $absolute);
+    }
+
+    /**
+     * Generate the URL for same route name but different locale
+     *
+     * @param  string  $name
+     * @param  array   $parameters
+     * @param  bool    $absolute
+     * @return string
+     */
+    function other_route($name, $parameters = [], $absolute = true)
+    {
+        return app('locale-url')->localeRoute(null, $name, $parameters, $absolute);
     }
 }
