91d11b168284abcb1267e1cd8d168814c5a4c5d7
CaribouFuté <frederic.chiasson@gmail.com>
2016-11-24 16:26:08 -0500

    Finished making addLocale method

--- src/Routing/CaribouRouter.php
+++ src/Routing/CaribouRouter.php
@@ -2,16 +2,22 @@
 
 namespace CaribouFute\LocaleRoute\Routing;
 
+use CaribouFute\LocaleRoute\Locale\Route as LocaleRoute;
+use CaribouFute\LocaleRoute\Locale\Url as LocaleUrl;
 use Illuminate\Contracts\Routing\Registrar as IlluminateRouter;
 use Illuminate\Routing\Route;
 
 class CaribouRouter
 {
     protected $router;
+    protected $url;
+    protected $route;
 
-    public function __construct(IlluminateRouter $router)
+    public function __construct(IlluminateRouter $router, LocaleUrl $url, LocaleRoute $route)
     {
         $this->router = $router;
+        $this->url = $url;
+        $this->route = $route;
     }
 
     public function getRouter()
@@ -19,7 +25,7 @@ class CaribouRouter
         return $this->router;
     }
 
-    protected function addActionLocale(Route $route)
+    public function addLocale(Route $route)
     {
         $locale = $this->getActionLocale($route);
 
@@ -27,8 +33,8 @@ class CaribouRouter
             return $route;
         }
 
-        //Switch route name locale
-        //Switch route url locale
+        $route = $this->switchRouteLocale($locale, $route);
+        $route = $this->switchUrlLocale($locale, $route);
 
         return $route;
     }
@@ -49,6 +55,30 @@ class CaribouRouter
         $action = $route->getAction();
 
         return is_array($action) && isset($action['locale']) ? $action['locale'] : null;
+    }
+
+    public function switchRouteLocale($locale, Route $route)
+    {
+        $name = $route->getName();
+        if (!$name) {
+            return $route;
+        }
+
+        $name = $this->route->switchLocale($locale, $name);
+
+        $action = $route->getAction();
+        $action['as'] = $name;
+        $route->setAction($action);
+
+        return $route;
+    }
+
+    public function switchUrlLocale($locale, Route $route)
+    {
+        $uri = $this->url->switchLocale($locale, $route->uri());
+        $route->setUri($uri);
+
+        return $route;
     }
 
     public function __call($method, $arguments)
--- tests/Unit/Routing/CaribouRouterTest.php
+++ tests/Unit/Routing/CaribouRouterTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Unit\Routing;
 
+use CaribouFute\LocaleRoute\Locale\Route as LocaleRoute;
+use CaribouFute\LocaleRoute\Locale\Url;
 use CaribouFute\LocaleRoute\Routing\CaribouRouter;
 use Illuminate\Routing\Route;
 use Illuminate\Routing\Router as IlluminateRouter;
@@ -14,7 +16,85 @@ class ClassTest extends TestCase
     {
         parent::setUp();
         $this->illuminateRouter = Mockery::mock(IlluminateRouter::class);
-        $this->router = new CaribouRouter($this->illuminateRouter);
+        $this->url = Mockery::mock(Url::class);
+        $this->route = Mockery::mock(LocaleRoute::class);
+        $this->router = Mockery::mock(CaribouRouter::class, [$this->illuminateRouter, $this->url, $this->route])->makePartial();
+    }
+
+    public function testAddLocaleDoesNothingWhenNoLocale()
+    {
+        $route = Mockery::mock(Route::class);
+        $this->router->shouldReceive('getActionLocale')->with($route)->once()->andReturn(null);
+
+        $testRoute = $this->router->addLocale($route);
+
+        $this->assertSame($route, $testRoute);
+    }
+
+    public function testAddLocaleChangesRouteAndUrl()
+    {
+        $locale = 'fr';
+        $uri = 'uri';
+        $newUri = $locale . '/' . $uri;
+
+        $name = 'route';
+        $newName = $locale . '.' . $name;
+
+        $action = ['locale' => $locale, 'as' => $name, 'uses' => 'Controller@action'];
+
+        $route = Mockery::mock(Route::class);
+
+        $this->router->shouldReceive('getActionLocale')->with($route)->once()->andReturn($locale);
+        $this->router->shouldReceive('switchRouteLocale')->with($locale, $route)->once()->andReturn($route);
+        $this->router->shouldReceive('switchUrlLocale')->with($locale, $route)->once()->andReturn($route);
+
+        $testRoute = $this->router->addLocale($route);
+
+        $this->assertSame($route, $testRoute);
+
+    }
+
+    public function testSwitchRouteLocaleDoesNothingWhenNoName()
+    {
+        $route = Mockery::mock(Route::class);
+        $route->shouldReceive('getName')->once()->andReturn(null);
+
+        $testRoute = $this->router->switchRouteLocale('fr', $route);
+
+        $this->assertSame($route, $testRoute);
+    }
+
+    public function testSwitchRouteLocaleChangesRouteName()
+    {
+        $locale = 'fr';
+        $name = 'route';
+        $newName = $locale . '.' . $name;
+        $route = Mockery::mock(Route::class);
+        $route->shouldReceive('getName')->once()->andReturn($name);
+
+        $this->route->shouldReceive('switchLocale')->with($locale, $name)->once()->andReturn($newName);
+        $route->shouldReceive('getAction')->once()->andReturn([]);
+        $route->shouldReceive('setAction')->with(['as' => $newName])->once();
+
+        $testRoute = $this->router->switchRouteLocale($locale, $route);
+
+        $this->assertSame($route, $testRoute);
+    }
+
+    public function testSwitchUrlLocale()
+    {
+        $locale = 'fr';
+        $uri = 'uri';
+        $newUri = $locale . '/' . $uri;
+
+        $route = Mockery::mock(Route::class);
+        $route->shouldReceive('uri')->once()->andReturn($uri);
+        $this->url->shouldReceive('switchLocale')->with($locale, $uri)->once()->andReturn($newUri);
+        $route->shouldReceive('setUri')->with($locale . '/' . $uri)->once();
+
+        $testRoute = $this->router->switchUrlLocale($locale, $route);
+
+        $this->assertSame($route, $testRoute);
     }
 
     public function testGetActionLocalesReturnNullWhenNoLocaleKey()
@@ -69,18 +149,5 @@ class ClassTest extends TestCase
         $route->shouldReceive('getAction')->once()->andReturn($action);
 
         $this->assertSame($locale, $this->router->getActionLocale($route));
-    }
-
-    public function testGet()
-    {
-        $url = 'url';
-        $action = 'Controller@action';
-        $route = Mockery::mock(Route::class);
-
-        $this->illuminateRouter->shouldReceive('get')->with($url, $action)->once()->andReturn($route);
-
-        $this->router->get($url, $action);
-
-        $this->assertTrue(true);
     }
 }
