218e0d3327a52b73c7d372fe6ee6d64d2ec8606f
CaribouFuté <frederic.chiasson@gmail.com>
2016-11-27 13:07:52 -0500

    Finished LocaleRoute::resource

--- src/Routing/ResourceRegistrar.php
+++ src/Routing/ResourceRegistrar.php
@@ -16,18 +16,45 @@ class ResourceRegistrar extends IlluminateResourceRegistrar
         parent::__construct($router);
     }
 
-    protected function addResourceIndex($name, $base, $controller, $options)
-    {
-        $uri = $this->getResourceUri($name);
-        $rawRouteName = $this->getResourceName($name, 'index', $options);
-        $action = $this->getLocaleResourceAction($controller, 'index');
-
-        return $this->localeRouter->get($rawRouteName, $action, $uri);
-    }
-
     protected function getLocaleResourceAction($controller, $method)
     {
         return $controller . '@' . $method;
+    }
+
+    protected function addResourceIndex($name, $base, $controller, $options)
+    {
+        $uri = $this->getResourceUri($name);
+        $name = $this->getResourceName($name, 'index', $options);
+        $action = $this->getLocaleResourceAction($controller, 'index');
+
+        return $this->localeRouter->get($name, $action, $uri);
+    }
+
+    protected function addResourceCreate($name, $base, $controller, $options)
+    {
+        $uri = $this->getResourceUri($name) . '/create';
+        $name = $this->getResourceName($name, 'create', $options);
+        $action = $this->getLocaleResourceAction($controller, 'create');
+
+        return $this->localeRouter->get($name, $action, $uri);
+    }
+
+    protected function addResourceShow($name, $base, $controller, $options)
+    {
+        $uri = $this->getResourceUri($name) . '/{' . $base . '}';
+        $name = $this->getResourceName($name, 'show', $options);
+        $action = $this->getLocaleResourceAction($controller, 'show');
+
+        return $this->localeRouter->get($name, $action, $uri);
+    }
+
+    protected function addResourceEdit($name, $base, $controller, $options)
+    {
+        $uri = $this->getResourceUri($name) . '/{' . $base . '}/edit';
+        $name = $this->getResourceName($name, 'edit', $options);
+        $action = $this->getLocaleResourceAction($controller, 'edit');
+
+        return $this->localeRouter->get($name, $action, $uri);
     }
 
 }
