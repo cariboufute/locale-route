fe4a8ba5c16785ae2b2e60bf87cb3180d7d8b7a2
CaribouFuté <frederic.chiasson@gmail.com>
2017-02-10 10:50:16 -0500

    Added space before deux-points for Scrutinizer

--- src/Routing/ResourceRegistrar.php
+++ src/Routing/ResourceRegistrar.php
@@ -30,9 +30,9 @@ class ResourceRegistrar extends IlluminateResourceRegistrar
 
     /**
      *  Get resource name for both Laravel 5.4 (getResourceRouteName) and Laravel <5.4 (getResourceName)
-     *  @param string $resource: the resource name
-     *  @param string $method: the controller method
-     *  @param array $options: different options
+     *  @param string $resource : the resource name
+     *  @param string $method : the controller method
+     *  @param array $options : different options
      *  @return string
      */
     protected function getResourceName($resource, $method, $options)
